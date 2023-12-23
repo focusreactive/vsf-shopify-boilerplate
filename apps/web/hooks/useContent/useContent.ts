65b46112f55a6c9f49221b9e3ace0944e697e2df
Oleg Proskurin <regx@usul.su>
2023-12-24 01:05:28 +0700

    Add get Pages query

--- apps/web/hooks/useContent/useContent.ts
+++ apps/web/hooks/useContent/useContent.ts
@@ -1,6 +1,87 @@
 import { QueryClient, useQuery } from '@tanstack/react-query';
 import { sdk } from '~/sdk';
 
+const query = `#graphql
+getPages {
+  pages(first: 50) {
+    edges {
+      node {
+        id
+        title
+        handle
+        seo {
+          title
+          description
+        }
+        content: metafield(namespace: "custom", key: "content") {
+          id
+          key
+          namespace
+          value
+          references(first: 50) {
+            edges {
+              node {
+                __typename
+                ... on Metaobject {
+                  id
+                  type
+                  fields {
+                    type
+                    key
+                    value
+                    reference {
+                      __typename
+                      ... on Collection {
+                        id
+                        title
+                        slug: handle
+                      }
+                      ... on Product {
+                        id
+                        title
+                        slug: handle
+                      }
+                      ... on Metaobject {
+                        id
+                        handle
+                        fields {
+                          key
+                          type
+                          value
+                          reference {
+                            __typename
+                            ... on Page {
+                              id
+                              title
+                              slug: handle
+                            }
+                            ... on MediaImage {
+                              id
+                              alt
+                              image {
+                                url
+                                altText
+                                width
+                                height
+                                id
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
 export async function prefetchContent(url: string): Promise<QueryClient> {
   const queryClient = new QueryClient();
   // TODO [>0.2]: revert and switch to Shopify SDK
